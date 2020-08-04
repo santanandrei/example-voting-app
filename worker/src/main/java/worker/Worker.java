75dc8dab48f12e6f35c1a7c4a4d28b9f393dc93a
Andrei Santana <andrei.costa@ibm.com>
2020-08-04 16:07:22 -0300

    authentication redis

--- worker/src/main/java/worker/Worker.java
+++ worker/src/main/java/worker/Worker.java
@@ -50,7 +50,7 @@ class Worker {
 
     while (true) {
       try {
-        conn.keys("redis_password");
+        conn.keys("*");
         break;
       } catch (JedisConnectionException e) {
         System.err.println("Waiting for redis");
